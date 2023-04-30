d3f1015dce7474ae476e2e19f8da7f985bed99a8
Thiago Granja <thiagoaagranja@outlook.com>
2023-04-30 02:25:28 -0300

    added imports on RecordsTable.jsx

--- dindin/src/components/RecordsTable/index.jsx
+++ dindin/src/components/RecordsTable/index.jsx
@@ -1,4 +1,8 @@
-import './styles.css'
+import './styles.css';
+import Filter from './Filter';
+import Listing from './Listing';
+import Summary from './Summary';
+import TableHeader from './TableHeader';
 
 export default function RecordsTable() {
 
