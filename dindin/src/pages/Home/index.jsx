0f82ae941e46e7294e978e7aeefc916c26200033
Igor Jardim Bahia <igorjbahia@gmai.com>
2023-05-03 07:19:17 -0300

    :sparkles: feat: AdicionAdded more colors to the global root of the CSS. Created additional icons in the assets. Work in progress for the creation of the home page.

--- dindin/src/pages/Home/index.jsx
+++ dindin/src/pages/Home/index.jsx
@@ -5,10 +5,38 @@ export default function Home() {
   return (
     <div className='home'>
       <header>
-        <div className='logo'></div>
+        <a className='logo-link' href="#">
+          <div className='logo' alt='Logo icon'></div>
+        </a>
+        <div className='profile'>
+          <div className='profile-options'>
+            <a className='profile-image-link' href="#">
+              <div className='profile-image' alt='Profile picture'></div>
+            </a>
+            <a className='profile-name-link' href="#">
+              <h1 className='profile-name' alt='Profile name'>Daniel</h1>
+            </a>
+          </div>
+          <a className='profile-name-link'
+            // go to login page
+            href=""
+          >
+            <div className='sign-out-icon' alt='Logout icon'></div>
+          </a>
+        </div>
       </header>
-      <main>
 
+      <main>
+        <div className='account-details-container'>
+          <div className='filter-container' alt='Filter'>
+            <div className='filter-icon' alt='Filter icon'></div>
+            <div className='filter-text'>Filtrar</div>
+          </div>
+          <div className='account-summary-transactions'>
+            <div className='transactions'></div>
+            <div className='summary'></div>
+          </div>
+        </div>
       </main>
     </div>
   )
