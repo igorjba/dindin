4e6a9cbe67576d59aa2d5ea0e71f5e667e056bce
Igor Jardim Bahia <igorjbahia@gmai.com>
2023-05-04 18:10:00 -0300

    :sparkles: feat: Started creating the bank information.

--- dindin/src/pages/Home/index.jsx
+++ dindin/src/pages/Home/index.jsx
@@ -34,7 +34,24 @@ export default function Home() {
           </div>
           <div className='account-summary-transactions'>
             <div className='transactions'></div>
-            <div className='summary'></div>
+            <div className='sidebar'>
+              <div className='summary'>
+                <h1 className='summary-tittle'>Resumo</h1>
+                <div className='inflows-container'>
+                  <h3 className='inflows'>Entradas</h3>
+                  <h3 className='inflows-value'>R$200,00</h3>
+                </div>
+                <div className='outflows-container'>
+                  <h3 className='outflows'>Saídas</h3>
+                  <h3 className='outflows-value'>R$70,00</h3>
+                </div>
+                <div className='balance-container'>
+                  <h3 className='balance'>Saldo</h3>
+                  <h3 className='balance-value'>R$129,50</h3>
+                </div>
+              </div>
+              <button className='btn-add-record'>Adicionar Registro</button>
+            </div>
           </div>
         </div>
       </main>
