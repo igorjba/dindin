53e3e4a785a67fc9d3d087145e57c3793d14c645
Thiago Granja <thiagoaagranja@outlook.com>
2023-05-05 13:17:11 -0300

    feat: created signin and signup pages layout, lacking api logic and proper navigation using buttons

--- dindin/src/App.jsx
+++ dindin/src/App.jsx
@@ -1,11 +1,14 @@
 import './styles.css';
 import { BrowserRouter } from 'react-router-dom';
 import MainRoutes from './routes/routes.jsx';
+import SignUpForm from './components/SignUpForm';
+import OutsideBackground from './assets/bg-out.svg';
+import InsideBackground from './assets/bg-in.svg';
 
 export default function App() {
 
   return (
-    <div className='app'>
+    <div className='app' style={{backgroundImage: `url(${OutsideBackground})`}}> {/* check how bg will be rendered */}
       <BrowserRouter>
         <MainRoutes />
       </BrowserRouter>
--- dindin/src/components/SignUpForm/index.jsx
+++ dindin/src/components/SignUpForm/index.jsx
@@ -1,10 +1,66 @@
+import { Link, Navigate } from 'react-router-dom';
+import { useState } from 'react';
 import './styles.css'
 
 export default function SignUpForm() {
 
+  const [signUp, setSignUp] = useState({name: '', email:'', password:'', pwcheck:''});
+  const [error, setError] = useState('');
+  const [success, setSuccess] = useState(false);
+
+  function handleInput(event) {
+    const signUpInfo = {...signUp, [event.target.name]: event.target.value};
+    setSignUp(signUpInfo);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setError('');
+
+
+    if (!signUp.name) return setError('O campo nome é obrigatório');
+    if (!signUp.email) return setError('O campo e-mail é obrigatório');
+    if(!signUp.password) return setError('O campo senha é obrigatório');
+    if(!signUp.pwcheck || signUp.pwcheck != signUp.password) return setError('As senhas não conferem');
+
+    // try {chamar api para cadastrar usuário}
+    // catch(error) {return feedback visual falha}
+    setSuccess(true);
+    // feedback visual sucesso (imagem: Cadastro efetuado com sucesso! Você será redirecionado para a página de login.)
+    
+    setTimeout(() => {
+      //navigate to '/'
+    }, 1000);
+  }
+
   return (
-      <div className=''>
-        <h1>SignUpForm</h1>
+    <form className='signup-form'>
+      <h1>Cadastre-se</h1>
+      
+      <div className='input-container'>
+        <label htmlFor='name'>Nome</label>
+        <input type='text' name='name' id='name' onChange={ handleInput }/>
       </div>
+
+      <div className='input-container'>
+        <label htmlFor='email'>E-mail</label>
+        <input type='email' name='email' id='email' onChange={ handleInput }/>
+      </div>
+
+      <div className='input-container'>
+        <label htmlFor='password'>Senha</label>
+        <input type='password' name='password' id='password' onChange={ handleInput }/>
+      </div>
+
+      <div className='input-container'>
+        <label htmlFor='pwcheck'>Confirmação de senha</label>
+        <input type='password' name='pwcheck' id='pwcheck' onChange={ handleInput }/>
+      </div>
+
+      <button type='button' onClick={ handleSubmit }>Cadastrar</button>
+      <Link to='/'>Já tem cadastro? Clique aqui!</Link>
+      {error && <span>{error}</span>}
+      
+    </form>
   )
 };
